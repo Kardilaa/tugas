17459e7578515c8acbed75ffb96624925081753c
Kardilaa <110007841+Kardilaa@users.noreply.github.com>
2022-08-11 11:02:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+ $db = new PDO("mysql:host=localhost;dbname=ppdb",'root','');
+ $query = $db->query('DELETE FROM `calon_siswa` WHERE id = 333');
+
+ while ($data = $query->fetch() ) :?>
+    <p>
+        <a href="edit.php?id=<?= $data['id']; ?>">
+            <?= $data['nama'] ?>
+        </a>
+        
+        <a href="proses_hapus.php?id=<?= $data['id']; ?>" style="color:white;background:red;padding:2px;border-radius:3px">
+            Hapus
+        </a>
+    </p>
+<?php endwhile ?>
+
--- proses_tambah2.php
+++ proses_tambah2.php
@@ -1,7 +1,16 @@
-<?php
-$nama = $_GET['nama'];
-$kelas = $_GET['kelas'];
-//echo ini proses tambah
-$database = new PDO('mysql:host=localhost;dbname=mutujaya','root','');
-$query = $database->query("insert into siswa values('','$nama','$kelas')");
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
 
+<body>
+    <form method="get" action="proses_hapus.php">
+        <p>id <input name="id" type="number"></p>
+        <p>nama <input name="nama" type="text"></p>
+        <p>asal<input name="asal" type="text"></p>
+        <button type="submit">Simpan</button>
+
+    </form>
+
+</body>
+
+</html>
