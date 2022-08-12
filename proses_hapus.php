778b24285b6b2b87dd4e965f83ccb798ce15a7f6
Kardilaa <110007841+Kardilaa@users.noreply.github.com>
2022-08-12 09:23:36 +0700

    Delete proses_hapus.php

--- proses_hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-//koneksi
-$id = $_GET['id'];
-$nama = $_GET['nama'];
-$asal = $_GET['asal'];
-
-$database = new PDO("mysql:host=localhost; dbname=ppdb", "root", "");
-$query = $database->query("DELETE FROM `calon_siswa` WHERE id = 333('$id','$nama','$asal')");
